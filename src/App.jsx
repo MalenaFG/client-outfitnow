6da383502d4d662fe0365b67b683621a46294289
MalenaFG <malena.f.giusti@gmail.com>
2024-08-20 13:56:32 +0200

    routes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import AppRoutes from './routes/appRoutes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>soy tu app</h1>
-    </>
+    <div className='App'>
+      <AppRoutes />
+    </div>
   )
 }
 
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '../pages/HomePage/HomePage'
+import UsersListPage from '../pages/UsersListPage/UsersListPage'
+import MyProfilePage from '../pages/MyProfilePage/MyProfilePage'
+import AboutUsPage from '../pages/AboutUsPage/AboutUsPage'
+import ServicesListPage from '../pages/ServicesListPage/ServicesListPage'
+import StylistsListPage from '../pages/StylistsListPage/StylistsListPage'
+import StylistDetailsPage from '../pages/StylistDetailsPage/StylistDetailsPage'
+import ServiceDetailsPage from '../pages/ServiceDetailsPage/ServiceDetailsPage'
+
+const AppRoutes = () => {
+    return (
+        <div className="AppRoutes">
+            <Routes>
+                <Route path={`/`} element={<HomePage />} />
+                <Route path={'/services'} element={<ServicesListPage />} />
+                <Route path={'/services/:serviceId'} element={<ServiceDetailsPage />} />
+                <Route path={'/stylists'} element={<StylistsListPage />} />
+                <Route path={'/stylists/:stylistId'} element={<StylistDetailsPage />} />
+                <Route path={'/profile/:userId'} element={<MyProfilePage />} />
+                <Route path={'/aboutus'} element={<AboutUsPage />} />
+                <Route path={'/users'} element={<UsersListPage />} />
+                <Route path={'*'} element={<h1>404</h1>} />
+            </Routes>
+        </div>
+    )
+}
+
+export default AppRoutes
